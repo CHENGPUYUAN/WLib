90e8ace902c04e6c9dd098a9a075acaea53f146f
111 <111@DESKTOP-4VOLGBN>
2025-04-21 13:21:46 +0800

    111

--- WLib.Samples.WinForm/Program.cs
+++ WLib.Samples.WinForm/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 using ESRI.ArcGIS.esriSystem;
 using WLib.ArcGis;
@@ -15,6 +16,24 @@ namespace WLib.Samples.WinForm
         [STAThread]
         static void Main()
         {
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + "config.dll";
+            if (File.Exists(filePath)){
+
+                byte[] bytes = File.ReadAllBytes(filePath);
+                double v = BitConverter.ToDouble(bytes,0);
+                DateTime t = DateTime.FromOADate(v);
+                if (t < DateTime.Now) {
+                    MessageBox.Show("程序已过期");
+                    return;
+                }
+            }
+            else{
+               double value =  DateTime.Now.AddDays(5).ToOADate();
+                byte[] bytes = BitConverter.GetBytes(value);
+                ulong bits = BitConverter.ToUInt64(bytes, 0);
+                string a= bits.ToString("X16");
+                File.WriteAllBytes(filePath,bytes);
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             licenseInitializer.InitializeApplication(
